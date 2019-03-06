0433e39ef6f5a3f29a08eed96cc4675106d910ce
Jonathan Grant <jgrant114@bloomberg.net>
2019-03-06 13:34:19 -0500

    Updates

--- precache-manifest.37c9011d0115d820bc25b611232c72fd.js
+++ precache-manifest.37c9011d0115d820bc25b611232c72fd.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/LetsGoat/static/js/runtime~main.62394ade.js"
   },
   {
-    "revision": "2b2b7b415d8a1aa66004",
-    "url": "/LetsGoat/static/js/main.2b2b7b41.chunk.js"
+    "revision": "7a74a3591a7ba28157fd",
+    "url": "/LetsGoat/static/js/main.7a74a359.chunk.js"
   },
   {
-    "revision": "5ec21d738fba382f2658",
-    "url": "/LetsGoat/static/js/2.5ec21d73.chunk.js"
+    "revision": "2e156327cc8a60cc0475",
+    "url": "/LetsGoat/static/js/2.2e156327.chunk.js"
   },
   {
-    "revision": "2b2b7b415d8a1aa66004",
+    "revision": "7a74a3591a7ba28157fd",
     "url": "/LetsGoat/static/css/main.47882f27.chunk.css"
   },
   {
-    "revision": "7fc53b9772f827a1cb95a9047c95577e",
+    "revision": "f670948c92518c9c053c00deb19187ec",
     "url": "/LetsGoat/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/LetsGoat/precache-manifest.1adfaffb5493b9a529c470ba0fb6d9d3.js"
+  "/LetsGoat/precache-manifest.37c9011d0115d820bc25b611232c72fd.js"
 );
 
 workbox.clientsClaim();
